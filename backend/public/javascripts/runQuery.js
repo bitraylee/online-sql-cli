e26deb3d70c0e6cba6a2478d189a1263d2248336
bitraylee <anshumohanta@hotmail.com>
2022-06-22 09:18:41 +0530

    Adding more files to previous commit

--- backend/public/javascripts/runQuery.js
+++ backend/public/javascripts/runQuery.js
@@ -2,42 +2,19 @@
 
 const { response } = require("express")
 const mysql = require("mysql2")
-const credentials = require("..credentials")
-// const formatDate = require("../services/formatDate")
+const credentials = require("../../credentials")
 
 const runQuery = (query) => {
 	return new Promise((resolve, reject) => {
 		const connection = mysql.createConnection(credentials)
 		if (!connection) connection.connect()
 		else if (connection) {
-			const rowData = {
-				selectedData: [],
-				selectedRows: 0,
-			}
 			connection.query(query, (err, rows, fields) => {
 				if (err) reject(err)
 
 				if (rows != undefined) {
-					rowData.selectedData = rows.map((row) => {
-						row.clear_date = formatDate(row.clear_date)
-						row.posting_date = formatDate(row.posting_date)
-						row.document_create_date = formatDate(row.document_create_date)
-						row.document_create_date1 = formatDate(row.document_create_date1)
-						row.due_in_date = formatDate(row.due_in_date)
-						row.baseline_create_date = formatDate(row.baseline_create_date)
-
-						return row
-					})
+					resolve(rows)
 				} else reject("No rows selected")
-
-				const getRowNumQuery = `SELECT COUNT(*) FROM \`winter_internship\` WHERE cust_number LIKE "${searchInput + "%"}" ORDER BY ${field} ${sort.toUpperCase()}`
-
-				connection.query(getRowNumQuery, (err, rows, fields) => {
-					if (err) reject(err)
-
-					rowData.selectedRows = rows[0]["COUNT(*)"]
-					resolve(rowData)
-				})
 			})
 		} else reject("Unable to fetch data")
 	})
--- backend/routes/runSQL.js
+++ backend/routes/runSQL.js
@@ -1,7 +1,8 @@
 // * For checking what kind of query it is
 "use strict"
+// const { response } = require("express")
 const express = require("express")
-
+const runQuery = require("../public/javascripts/runQuery")
 let router = express.Router()
 router.use(express.json({ extended: false }))
 
@@ -11,8 +12,15 @@ router
 		const query = req.query.query
 		var querySplit = query.split(" ")
 		console.log(query, querySplit[0])
-
-		res.send(querySplit[0])
+		runQuery(query)
+			.then((response) => {
+				res.send(response)
+			})
+			.catch((e) => {
+				console.log(e)
+				res.send([{ Message: "Unable to process the query" }])
+			})
+		// res.send(querySplit[0])
 	})
 	.post((req, res) => {})
 
